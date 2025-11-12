6fc926748443c6dd29e3f29c2bc06dd9dfa163f8
jkeele <jkeele@bsd220.org>
2025-11-12 10:38:50 -0600

    Initial Commit

---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Jensen Keele on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
