34a84215082736be1dfddfe9214109c2df1b447b
jkeele <jkeele@bsd220.org>
2025-11-18 10:55:09 -0600

    added navigation view

--- Rock Paper Scissors/ContentView.swift
+++ Rock Paper Scissors/ContentView.swift
@@ -9,22 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Rock Paper Scissors")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .bold()
+        NavigationView {
+            VStack {
+                Text("Rock Paper Scissors")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .bold()
+                Text("Player 1 Turn")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                HStack {
+                    SelectionColumns(image: "rock", width: 110, height: 150)
+                    SelectionColumns(image: "paper", width: 110, height: 150)
+                    SelectionColumns(image: "scissor", width: 150, height: 100)
+                }
+            }
+            NavigationLink("Directions", destination: InstructionsView())
         }
     }
-}
-struct SelectionColumns: View {
-    let color: Color
-    let image: String
-    var body: some View {
-        ZStack {
-            Image("\(image)").resizable().frame(width: 100, height: 100).foregroundColor(.clear)
+    struct CustomText: View {
+        let words: String
+        var body: some View {
+            Text("\(words)")
+                .font(.default,)
+                .fontWeight(.light)
         }
-        .frame(width: 100, height: 100)
+    }
+    struct SelectionColumns: View {
+        let image: String
+        let width: Int
+        let height: Int
+        var body: some View {
+            ZStack {
+                
+                Image("\(image)").resizable().frame(width: CGFloat(width), height: CGFloat(height)).foregroundColor(.clear)
+                //ask mr.B about CGFloat
+            }
+            .frame(width: 100, height: 100)
+        }
+    }
+    struct InstructionsView: View {
+        var body: some View {
+            VStack {
+                Text("In Rock Paper Scissors, you compete against another person. Rock beats scissors, scissors beat paper, and paper beats rock.")
+                Text("Player 1 will make your move first. You can choose to play Rock, Paper, or Scissors. After Player 1 decides their move, ")
+            }
+        }
     }
 }
 #Preview {
