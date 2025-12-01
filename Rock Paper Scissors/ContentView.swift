126d0d36f9ca892a71f9c672b0bb6b235d616a3e
jkeele <jkeele@bsd220.org>
2025-12-01 10:53:50 -0600

    added navigation links to player 2 screen

--- Rock Paper Scissors/ContentView.swift
+++ Rock Paper Scissors/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var player1Choice = ""
+    @State private var player2Choice = ""
     var body: some View {
         NavigationView {
             VStack {
@@ -16,10 +18,19 @@ struct ContentView: View {
                     .font(.title2)
                     .fontWeight(.heavy)
                 HStack {
-                    SelectionColumns(image: "rock", width: 110, height: 150)
-                    SelectionColumns(image: "paper", width: 110, height: 150)
-                    SelectionColumns(image: "scissor", width: 150, height: 100)
+                    ImageColumns(image: "rock", width: 110, height: 150)
+                    ImageColumns(image: "scissor", width: 150, height: 100)
+                    ImageColumns(image: "paper", width: 110, height: 150)
                 }
+                HStack (alignment: .center){
+                    NavigationLink("Rock", destination: Player2())
+                        .padding()
+                    NavigationLink("Scissors", destination: Player2())
+                        .padding()
+                    NavigationLink("Paper", destination: Player2())
+                        .padding()
+                }
+                
                 NavigationLink("Directions", destination: InstructionsView())
                     .font(.headline)
                     .padding()
@@ -35,13 +46,15 @@ struct ContentView: View {
                 .bold()
         }
     }
-    struct SelectionColumns: View {
+    struct ImageColumns: View {
         let image: String
         let width: Int
         let height: Int
         var body: some View {
             ZStack {
-                Image("\(image)").resizable().frame(width: CGFloat(width), height: CGFloat(height)).foregroundColor(.clear)
+                Image("\(image)")
+                    .resizable()
+                    .frame(width: CGFloat(width), height: CGFloat(height))
                 //how can I put in integers for the width and height
             }
             .frame(width: 100, height: 100)
@@ -49,12 +62,14 @@ struct ContentView: View {
     }
     struct InstructionsView: View {
         var body: some View {
+            CustomText(words: "How to Play")
+                .padding()
             VStack (alignment: .leading){
                 Text("In Rock Paper Scissors, you compete against another person.")
                     .padding()
                 Text("Rock beats scissors, scissors beat paper, and paper beats rock.")
                     .padding()
-                Text("Player 1 will make your move first. You can choose to play Rock, Paper, or Scissors.")
+                Text("Player 1 will make their move first. They can choose to play Rock, Paper, or Scissors.")
                     .padding()
                 Text("After Player 1 decides, press (placeholder) and pass the phone to Player 2.")
                     .padding()
---
+++ Rock Paper Scissors/Player2.swift
@@ -0,0 +1,18 @@
+//
+//  Player2.swift
+//  Rock Paper Scissors
+//
+//  Created by Jensen Keele on 11/20/25.
+//
+
+import SwiftUI
+
+struct Player2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Player2()
+}
