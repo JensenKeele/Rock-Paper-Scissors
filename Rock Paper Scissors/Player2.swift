6d123565fd9d27d87b4ce24f5d4e3638e9aaed1a
jkeele <jkeele@bsd220.org>
2025-12-02 10:26:31 -0600

    finished player 2 screen and added GameResult view

--- Rock Paper Scissors/Player2.swift
+++ Rock Paper Scissors/Player2.swift
@@ -9,10 +9,52 @@ import SwiftUI
 
 struct Player2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack{
+                CustomText(words: "Rock Paper Scissors")
+                Text("Player 2 Turn")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                HStack {
+                    ImageColumns(image: "rock", width: 110, height: 150)
+                    ImageColumns(image: "scissor", width: 150, height: 100)
+                    ImageColumns(image: "paper", width: 110, height: 150)
+                }
+                HStack (alignment: .center) {
+                    NavigationLink("Rock", destination: GameResult())
+                        .padding()
+                    NavigationLink("Scissors", destination: GameResult())
+                        .padding()
+                    NavigationLink("Paper", destination: GameResult())
+                        .padding()
+                }
+            }
+        }
     }
 }
-
+struct CustomText: View {
+    let words: String
+    var body: some View {
+        Text("\(words)")
+            .font(.largeTitle)
+            .fontWeight(.heavy)
+            .bold()
+    }
+}
+struct ImageColumns: View {
+    let image: String
+    let width: Int
+    let height: Int
+    var body: some View {
+        ZStack {
+            Image("\(image)")
+                .resizable()
+                .frame(width: CGFloat(width), height: CGFloat(height))
+            //how can I put in integers for the width and height
+        }
+        .frame(width: 100, height: 100)
+    }
+}
 #Preview {
     Player2()
 }
